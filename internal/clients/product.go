e39a966702bc337d430b397f1aa699b522b5fc44
Ignacio Sanchez Gines <nacho@geardome.com>
2020-09-13 15:13:50 +0200

    Improve error handling

--- internal/clients/product.go
+++ internal/clients/product.go
@@ -2,9 +2,11 @@ package clients
 
 import (
 	"log"
+	"time"
 
 	"github.com/drhelius/grpc-demo-proto/product"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 )
 
 var ProductService product.ProductServiceClient
@@ -12,7 +14,13 @@ var ProductService product.ProductServiceClient
 func init() {
 	log.Printf("[Order] Dialing to 'product:5000' ...")
 
-	conn, err := grpc.Dial("product:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true))
+	keepAliveParams := keepalive.ClientParameters{
+		Time:                5 * time.Second,
+		Timeout:             time.Second,
+		PermitWithoutStream: true,
+	}
+
+	conn, err := grpc.Dial("product:5000", grpc.WithInsecure(), grpc.WithBlock(), grpc.FailOnNonTempDialError(true), grpc.WithKeepaliveParams(keepAliveParams))
 	if err != nil {
 		log.Fatalf("[Order] Error dialing to Product service: %v", err)
 	}
